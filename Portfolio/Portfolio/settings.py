67127272d415542c3672029fc939d28408710503
Roberto Guerra <contact@robertoguerra.info>
2020-02-13 18:41:15 -0500

    Created basic Homepage, set up files and folders to allow the use of html, css, and images

--- Portfolio/Portfolio/settings.py
+++ Portfolio/Portfolio/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'homepage',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/images/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
